af7d657e6c5b79ab2049fa8e03f37d820d8d6a22
ericponce <eaponce@mit.edu>
2015-02-21 21:29:21 -0500

    Added application.h

--- firmware/spark-roboclaw.h
+++ firmware/spark-roboclaw.h
@@ -4,6 +4,7 @@
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include "application.h"
 
 /******************************************************************************
 * Definitions
